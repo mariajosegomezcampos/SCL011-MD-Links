2c7ad5eaed7f31dcb5d4aa17d4f206b1f3886426
Mariajose <mariajosegomezcampo@gmail.com>
2019-11-29 08:23:50 -0500

    intaladas dependecias,logro leer un archivo

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-module.exports = () => {
-  // ...
-};
+
+const fs = require('fs');
+const path = require('path');
+const markdownLinkExtractor = require('./src/linkExtractor')
+const fetch = require("node-fetch");
+
+
+
+
+ const router = readMdFile('../README.md');
+ console.log(process.argv);
+ console.log(require.main === module);
+//   const route = path.resolve("./README.md"); //vuelve la ruta ingresada por el user, una ruta absoluta
+//  console.log(route);
+
+
+//resolve cuando es positivo ,reject cuando es negativo 
+// Función que lee un archivo y retorna promesa con el contenido del archivo
+function readMdFile(filePath ){
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (error, archivo) => {
+
+      if (error) {
+
+       return reject(error) //// Sabemos que hay un error, así que rechazamos la promesa
+        // Si hay error, nos aseguramos con return de no seguir ejecutando nada más en esta función
+
+      }
+       return resolve(archivo).then((archivo) => {
+        linkExtractor(archivo)//funcion que extrae los links
+       
+        })
+
+      });
+    });
+  }
+
+
+
+
+// funcion que recorre los links
+function linkExtractor(archivo) {
+  
+    
+
+
+}
+
+
